41f6b933840f866feccc8a02eace1ff498ce060d
Jayden Sansom <jaydensansom@gmail.com>
2018-07-26 14:49:58 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -13,7 +13,7 @@ create_olympics_hash
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
   
-  summer_olympics.push(:Atlanta, "1996")
+  summer_olympics(:Atlanta, "1996")
   
   
 end
